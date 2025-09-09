7b5d27ba9d5bf58239db684cae4ea20b20f5ae16
nitinvayu <151741776+nitinvayu@users.noreply.github.com>
2025-09-10 00:00:03 +0530

    Time: 117 ms (80.65%) | Memory: 77.9 MB (91.98%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, key, val):
+        self.key  = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.map = {}
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def add(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            node = self.map[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            self.remove(self.map[key])
+        node = Node(key, value)
+        self.add(node)
+        self.map[key] = node
+
+        if len(self.map) > self.capacity:
+            lru = self.tail.prev
+            self.remove(lru)
+            del self.map[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
