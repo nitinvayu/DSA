e9235de5bdf95ad792734725734fc28b0b6d3a74
nitinvayu <151741776+nitinvayu@users.noreply.github.com>
2025-10-07 01:37:35 +0530

    Time: 19 ms (37.20%) | Memory: 20.8 MB (62.91%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp ={}
+        for s in strs:
+            f=str(sorted(s))
+            if f not in mp:
+                mp[f]=[]
+            mp[f].append(s)
+        return list(mp.values())
